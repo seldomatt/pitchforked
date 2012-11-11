d1eb1039dfeeafbcd336caae40459904ac373c04
Matthew Salerno <matthewsalern@gmail.com>
2012-11-11 12:28:27 -0500

    scrape errored out on review 13k

--- lib/pitchforked/scrapefork.rb
+++ lib/pitchforked/scrapefork.rb
@@ -12,8 +12,7 @@ require_relative 'label'
 reviewlinks = []
 n = 1
 doc = Nokogiri::HTML(open("http://pitchfork.com/reviews/albums/#{n}/"))
-# while doc.css(".next-container").text
-while n <= 2
+while doc.css(".next-container").text
   if doc.css(".next-container").text.include?("Next")
     doc.css(".object-grid ul li a").each do |review|
     reviewlinks << review["href"]
